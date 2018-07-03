280f25cc096337d5616d7705a32f436c64ef57f0
George <quicksilver-ga@hotmail.com>
2018-07-02 16:45:12 -1000

    Sort of finished

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,16 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+    var secretNum = 0;
+    var callAgain = false;
+
+    return function () {
+        if (callAgain === false) {
+            secretNum = Math.random() * 1000000;
+            callAgain = true;
+
+            return secretNum;
+        } else {
+            return secretNum;
+        }
+    }
 };
